246c9597a49f50e8c25b173e45fa9d05cac85147
sunshineplan <sunshineplan@users.noreply.github.com>
2021-11-17 16:06:35 +0800

    Add Forward Mail

---
+++ forwardMail.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/Azure/go-ntlmssp"
+	"github.com/sunshineplan/utils/mail"
+	"github.com/sunshineplan/utils/pop3"
+	"github.com/vharitonsky/iniflags"
+)
+
+var (
+	addr   = flag.String("addr", "", "Address")
+	domain = flag.String("domain", "", "Domain")
+	user   = flag.String("user", "", "Username")
+	pass   = flag.String("pass", "", "Password")
+	to     = flag.String("to", "", "Mail To")
+)
+
+var dialer = &mail.Dialer{Port: 587}
+
+var self string
+
+func init() {
+	var err error
+	self, err = os.Executable()
+	if err != nil {
+		log.Fatalln("Failed to get self path:", err)
+	}
+}
+
+func main() {
+	flag.StringVar(&dialer.Host, "host", "", "Mail Host Server")
+	flag.StringVar(&dialer.Account, "mail", "", "Mail Account")
+	flag.StringVar(&dialer.Password, "password", "", "Mail Account Password")
+	iniflags.SetConfigFile(filepath.Join(filepath.Dir(self), "config.ini"))
+	iniflags.SetAllowMissingConfigFile(true)
+	iniflags.SetAllowUnknownFlags(true)
+	iniflags.Parse()
+
+	if err := forwardMails(strings.Split(*to, ",")); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func forwardMails(to []string) error {
+	c, err := pop3.NewClient(*addr, true)
+	if err != nil {
+		return err
+	}
+	defer c.Quit()
+
+	if _, err := c.Cmd("AUTH NTLM", false); err != nil {
+		return err
+	}
+
+	b, err := ntlmssp.NewNegotiateMessage(*domain, "")
+	if err != nil {
+		return err
+	}
+
+	s, err := c.Cmd(base64.StdEncoding.EncodeToString(b), false)
+	if err != nil {
+		return err
+	}
+
+	b, err = base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return err
+	}
+	b, err = ntlmssp.ProcessChallenge(b, *user, *pass)
+	if err != nil {
+		return err
+	}
+
+	if _, err := c.Cmd(base64.StdEncoding.EncodeToString(b), false); err != nil {
+		return err
+	}
+
+	count, _, err := c.Stat()
+	if err != nil {
+		return err
+	}
+
+	for id := 1; id <= count; id++ {
+		s, err := c.Retr(id)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		if err := sendMail([]byte(s), to); err != nil {
+			log.Print(err)
+			continue
+		}
+
+		if err := c.Dele(id); err != nil {
+			log.Print(err)
+		}
+	}
+
+	return nil
+}
+
+func sendMail(b []byte, to []string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	return dialer.SendMail(ctx, dialer.Account, to, b)
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,11 @@
 module backup
 
-go 1.16
+go 1.17
 
 require (
-	github.com/sunshineplan/cipher v1.0.4
-	github.com/sunshineplan/utils v0.0.19
+	github.com/Azure/go-ntlmssp v0.0.0-20200615164410-66371956d46c
+	github.com/sunshineplan/utils v0.0.20
+	github.com/vharitonsky/iniflags v0.0.0-20180513140207-a33cd0b5f3de
 )
+
+require golang.org/x/crypto v0.0.0-20211108221036-ceb1ce70b4fa // indirect
